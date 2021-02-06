e20b9e939e4027b47dde1eb96740c042ffa460a6
Jacek Sawko <jaceksawko95@gmail.com>
2021-02-06 20:39:34 +0100

    initial commit

---
+++ src/test/java/com/js/securitytutorial/SecurityTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.js.securitytutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
