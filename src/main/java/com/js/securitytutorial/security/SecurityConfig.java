ab72396d9d1b28150c38af4f60ce6dd905f9c92a
Jacek <jaceksawko95@gmail.com>
2021-02-25 00:27:47 +0100

    Replaced Previous in memory Users, with ones that come from DB repository (it's not biggie, that repository is fake, Data layer is what matters)

--- src/main/java/com/js/securitytutorial/security/SecurityConfig.java
+++ src/main/java/com/js/securitytutorial/security/SecurityConfig.java
@@ -1,23 +1,19 @@
 package com.js.securitytutorial.security;
 
+import com.js.securitytutorial.auth.ApplicationUserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.concurrent.TimeUnit;
 
-import static com.js.securitytutorial.security.ApplicationUserPermission.*;
 import static com.js.securitytutorial.security.ApplicationUserRole.*;
 
 @Configuration
@@ -26,9 +22,11 @@ import static com.js.securitytutorial.security.ApplicationUserRole.*;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
+    private final ApplicationUserService applicationUserService;
 
-    public SecurityConfig(PasswordEncoder passwordEncoder) {
+    public SecurityConfig(PasswordEncoder passwordEncoder, ApplicationUserService applicationUserService) {
         this.passwordEncoder = passwordEncoder;
+        this.applicationUserService = applicationUserService;
     }
 
     @Override
@@ -64,28 +62,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
     @Bean
-    protected UserDetailsService userDetailsService() {
-        UserDetails student = User.builder()
-                                  .username("Jim")
-                                  .password(passwordEncoder.encode("password"))
-//                                  .roles(STUDENT.name()) //ROLE_STUDENT
-                                  .authorities(STUDENT.getGrantedAuthorities())
-                                  .build();
-
-        UserDetails admin = User.builder()
-                                .username("Michael")
-                                .password(passwordEncoder.encode("password123"))
-//                                .roles(ADMIN.name()) //ROLE_ADMIN
-                                .authorities(ADMIN.getGrantedAuthorities())
-                                .build();
-
-        UserDetails adminTrainee = User.builder()
-                                       .username("Dwight")
-                                       .password(passwordEncoder.encode("password123456"))
-//                                .roles(ADMIN_TRAINEE.name()) //ROLE_ADMINTRAINEE
-                                       .authorities(ADMIN_TRAINEE.getGrantedAuthorities())
-                                       .build();
-        return new InMemoryUserDetailsManager(student, admin, adminTrainee);
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(applicationUserService);
+        provider.setPasswordEncoder(passwordEncoder);
+        return provider;
     }
 }
