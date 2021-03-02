024174069ae22285d58b5cb76c294fca127fa0fb
Jacek <jaceksawko95@gmail.com>
2021-03-02 23:00:30 +0100

    Override successfulAuthentication method, so it creates JWT token after successful initial authentication

--- src/main/java/com/js/securitytutorial/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
+++ src/main/java/com/js/securitytutorial/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -1,6 +1,8 @@
 package com.js.securitytutorial.jwt;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -8,9 +10,13 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Date;
 
 @Component
 public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
@@ -35,5 +41,24 @@ public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePassword
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+        byte[] key = "secureVeryLongKeyToMakeItHarderForAnyoneElseToCrackThat".getBytes();
+
+        String token = Jwts.builder().setSubject(authResult.getName())
+                           .claim("authoritiess", authResult.getAuthorities())
+                           .setIssuedAt(new Date())
+                           .setExpiration(java.sql.Date.valueOf(LocalDate.now().plusDays(1)))
+                           .signWith(Keys.hmacShaKeyFor(key))
+                           .compact();
+
+        response.addHeader("Authorization", "Bearer " + token);
+
+        super.successfulAuthentication(request, response, chain, authResult);
     }
 }
